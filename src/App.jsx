be1a742fe6d6aa28a95cfde359f1e41e5780e7ad
Pedro Nicolás <pnicolas@frro.utn.edu.ar>
2025-09-29 19:57:35 -0300

    Cambio a TS

--- src/App.jsx
+++
@@ -1,9 +0,0 @@
-function App() {
-  return (
-    <div>
-      <h1>Frontend conectado al Backend </h1>
-    </div>
-  );
-}
-
-export default App;
---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { useEffect, useState } from "react";
+import './App.css';
+import axios from "axios";
+
+export function App() {
+  return (
+    <div>
+     <h1 className = "Titulo">Bienvenido a Niguta</h1>
+    </div>
+  );
+}
