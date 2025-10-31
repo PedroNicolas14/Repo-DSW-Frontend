de1b83d1d7fe1ef9266bf2616bde332a1820cfb0
Pedro Nicolás <pnicolas@frro.utn.edu.ar>
2025-10-31 03:49:06 -0300

    estilos agregados

--- src/components/verDetalle.tsx
+++ src/components/verDetalle.tsx
@@ -28,17 +28,19 @@ export function VerDetalle({ item, onClose }: VerDetalleProps) {
         <div className="VerDetalle-overlay" onClick={closeVerDetalle}>
           <div className="VerDetalle-content" onClick={(e) => e.stopPropagation()}>
             <img src={selectedItem.imagen} alt={selectedItem.nombre} className="indumentaria-imagen-VerDetalle" />
-            <h3>{selectedItem.categoria.nombre} {selectedItem.marca.nombre} {selectedItem.nombre}</h3>
-            <p><strong className="texto-ars">ARS</strong><strong className="precio-item">${selectedItem.precio}</strong></p>
-            <p><strong>Color:</strong> {selectedItem.color}</p>
-            <p><strong>Talle:</strong> {selectedItem.talle}</p>
-            <p><strong>Stock disponible:</strong> {selectedItem.stock}</p>
-            <p><strong>Nombre de la Marca:</strong> {selectedItem.marca.nombre}</p>
-            <p><strong>Descripción de la Marca:</strong> {selectedItem.marca.descripcion}</p>
-            <p><strong>Nombre de la Categoría:</strong> {selectedItem.categoria.nombre}</p>
-            <p><strong>Descripción de la Categoría:</strong> {selectedItem.categoria.descripcion}</p>
-            <button onClick={closeVerDetalle} className="boton-cerrar">Cerrar</button>
-            <button onClick={()=> agregarAlCarrito(selectedItem)}  className="boton-comprar">Agregar al Carrito</button>
+            <div className="verDetalle-info">
+              <h3>{selectedItem.categoria.nombre} {selectedItem.marca.nombre} {selectedItem.nombre}</h3>
+              <p><strong className="texto-ars">ARS</strong><strong className="precio-item">${selectedItem.precio}</strong></p>
+              <p><strong>Color:</strong> {selectedItem.color}</p>
+              <p><strong>Talle:</strong> {selectedItem.talle}</p>
+              <p><strong>Stock disponible:</strong> {selectedItem.stock}</p>
+              <p><strong>Nombre de la Marca:</strong> {selectedItem.marca.nombre}<img className="logo-marca" src={selectedItem.marca.logo} alt={selectedItem.marca.nombre}/></p>
+              <p><strong>Descripción de la Marca:</strong> {selectedItem.marca.descripcion}</p>
+              <p><strong>Nombre de la Categoría:</strong> {selectedItem.categoria.nombre}</p>
+              <p><strong>Descripción de la Categoría:</strong> {selectedItem.categoria.descripcion}</p>
+              <button onClick={closeVerDetalle} className="verDetalle-close">Cerrar</button>
+              <button onClick={()=> agregarAlCarrito(selectedItem)}  className="boton-comprar">Agregar al Carrito</button>
+            </div>
           </div>
         </div>
         )
